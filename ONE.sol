b6c2227d10977267592e66b3b3bb1877f93b418d
oneeth <oneeth@pm.me>
2021-02-05 12:22:19 +0800

    Offering.config[_quota_]

--- ONE.sol
+++ ONE.sol
@@ -81,25 +81,30 @@ contract Offering is Configurable {
 	using SafeMath for uint;
 	using SafeERC20 for IERC20;
 	
+	bytes32 internal constant _quota_      = 'quota';
+	bytes32 internal _quota_0              = '';            // placeholder
+	
 	IERC20 public token;
 	IERC20 public currency;
 	uint public price;
 	address public vault;
 	uint public begin;
 	uint public span;
+	mapping (address => uint) public offeredOf;
 	
-	function __Offering_init(address governor_, address _token, address _currency, uint _price, address _vault, uint _begin, uint _span) external initializer {
+	function __Offering_init(address governor_, address _token, address _currency, uint _price, uint _quota, address _vault, uint _begin, uint _span) external initializer {
 		__Governable_init_unchained(governor_);
-		__Offering_init_unchained(_token, _currency, _price, _vault, _begin, _span);
+		__Offering_init_unchained(_token, _currency, _price, _quota, _vault, _begin, _span);
 	}
 	
-	function __Offering_init_unchained(address _token, address _currency, uint _price, address _vault, uint _begin, uint _span) public governance {
+	function __Offering_init_unchained(address _token, address _currency, uint _price, uint _quota, address _vault, uint _begin, uint _span) public governance {
 		token = IERC20(_token);
 		currency = IERC20(_currency);
 		price = _price;
 		vault = _vault;
 		begin = _begin;
 		span = _span;
+		config[_quota_] = _quota;
 	}
 	
 	function offer(uint vol) external {
@@ -109,7 +114,9 @@ contract Offering is Configurable {
 				token.safeTransfer(vault, token.balanceOf(address(this)));
 			else
 				revert('offer over');
-		vol = Math.min(vol, token.balanceOf(address(this)));
+		require(offeredOf[msg.sender] < config[_quota_], 'out of quota');
+		vol = Math.min(Math.min(vol, config[_quota_].sub(offeredOf[msg.sender])), token.balanceOf(address(this)));
+		offeredOf[msg.sender] = offeredOf[msg.sender].add(vol);
 		uint amt = vol.mul(price).div(1e18);
 		currency.safeTransferFrom(msg.sender, address(this), amt);
 		currency.approve(vault, amt);
